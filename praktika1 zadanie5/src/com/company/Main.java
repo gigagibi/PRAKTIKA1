b4f5c26bff158dfc89affe2cf0ef2f0f0490fdce
gigagibi <70891118+gigagibi@users.noreply.github.com>
2020-09-07 13:58:47 +0300

    Add files via upload

---
+++ praktika1 zadanie5/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here\
+        System.out.format("%d%n", 1);
+        for(int i = 2; i < 11; i++)
+        {
+            System.out.format("1 / %d%n", i);
+        }
+    }
+}
